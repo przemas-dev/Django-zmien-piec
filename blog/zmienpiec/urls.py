b52a1fdde7c8bcc6de1ff68cb639b6a15dfd1f19
przemas-dev <62303978+przemas-dev@users.noreply.github.com>
2022-06-12 14:02:24 +0200

    add post list, article page

--- blog/zmienpiec/urls.py
+++ blog/zmienpiec/urls.py
@@ -1,6 +1,7 @@
-from . import views
+from .views import HomeView, ArticleDetailView
 from django.urls import path, include
 
 urlpatterns = [
-    path('', views.home, name="home"),
+    path('', HomeView.as_view(), name="home"),
+    path('article/<int:pk>', ArticleDetailView.as_view(), name='article')
 ]
