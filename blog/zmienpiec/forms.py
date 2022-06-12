834587cf2123b9a9775a724614664620404e8cd0
przemas-dev <62303978+przemas-dev@users.noreply.github.com>
2022-06-13 00:03:39 +0200

    correct adding comments

--- blog/zmienpiec/forms.py
+++ blog/zmienpiec/forms.py
@@ -1,6 +1,6 @@
-from dataclasses import fields
+from cProfile import label
 from django import forms
-from .models import Post
+from .models import Post, Comment
 
 
 class PostForm(forms.ModelForm):
@@ -12,4 +12,23 @@ class PostForm(forms.ModelForm):
             'title': forms.TextInput(attrs={'class': 'form-control'}),
             'body': forms.Textarea(attrs={'class': 'form-control'}),
             'author': forms.TextInput(attrs={'type': 'hidden', 'value': '', 'id': 'authorInput'}),
+        }
+        labels={
+            'title':'Tytuł',
+            'body':'Treść:'
+        }
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('author', 'body', 'post')
+
+        widgets={
+            'author': forms.TextInput(attrs={'type': 'hidden', 'value': '', 'id': 'authorInput'}),
+            'post': forms.TextInput(attrs={'type': 'hidden', 'value': '', 'id': 'postInput'}),
+            'body': forms.Textarea(attrs={'class': 'form-control'}),
+        }
+        labels={
+            'body':'',
         }
--- blog/zmienpiec/views.py
+++ blog/zmienpiec/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic.edit import FormMixin
 from .models import Post
-from .forms import PostForm
+from .forms import PostForm, CommentForm
+from django.urls import reverse
 
 
 class HomeView(ListView):
@@ -10,9 +12,30 @@ class HomeView(ListView):
     ordering = ['-create_date']
 
 
-class ArticleDetailView(DetailView):
+class ArticleDetailView(FormMixin, DetailView):
     model = Post
     template_name = 'article.html'
+    form_class = CommentForm
+
+    def get_success_url(self) -> str:
+        return reverse('article', args=(str(self.object.id)))
+
+    def get_context_data(self, **kwargs):
+        context = super(ArticleDetailView, self).get_context_data(**kwargs)
+        context['form'] = CommentForm(initial={'post': self.object})
+        return context
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        form = self.get_form()
+        if form.is_valid():
+            return self.form_valid(form)
+        else:
+            return self.form_invalid(form)
+
+    def form_valid(self, form):
+        form.save()
+        return super(ArticleDetailView, self).form_valid(form)
 
 
 class AddPostView(CreateView):
